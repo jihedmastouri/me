ff525f545bcd55dc432cd046a2bbe45b04ab55e5
Jihed Mastouri <jihedmastouri.inbox@gmail.com>
2023-09-30 17:42:42 +0100

    TimeToRead 101

---
+++ src/utils.ts
@@ -0,0 +1,21 @@
+import { getCollection } from 'astro:content';
+
+const posts = await getCollection('blog');
+
+export const tags = [
+  ...posts.reduce((acc, post) => {
+    post.data.tags?.forEach((tag) => acc.add(tag));
+    return acc;
+  }, new Set<string>()),
+];
+
+export const timeToRead = (content: string) => {
+  const clean = content.replace(/<\/?[^>]+(>|$)/g, '');
+  const noOfWords = clean.split(/\s/g).length;
+
+  const WPM = 200;
+  const minutes = noOfWords / WPM;
+
+  const readTime = Math.ceil(minutes);
+  return readTime;
+}

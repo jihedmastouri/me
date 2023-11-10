2aa6c84daefceaf8886484fa3769fe5af55be0a6
Jihed Mastouri <jihedmastouri.inbox@gmail.com>
2023-11-11 00:58:24 +0100

    in case my pc gets destroyed

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,7 +3,11 @@ module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      animation:{
+        'bounce-small': 'bounce cubic-bezier(0.175, 0.885, 0.32, 1.275) 0.5s infinite',
+      }
+    },
   },
   plugins: [
     require('@tailwindcss/typography'),
